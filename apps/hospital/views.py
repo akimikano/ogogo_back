8f1b5974281a989f10137a4ef87ddbece556271a
akimikano <akim.j.2002@gmail.com>
2022-10-08 16:48:45 +0600

    feat: worker actions

--- apps/hospital/views.py
+++ apps/hospital/views.py
@@ -1,6 +1,7 @@
 from rest_framework import views, viewsets, mixins
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from apps.hospital.serializers import *
 from apps.hospital.models import *
 from apps.users.models import *
@@ -32,5 +33,23 @@ class WorkerQueueView(viewsets.GenericViewSet, mixins.RetrieveModelMixin):
     queryset = User.objects.prefetch_related('hospitalunit_set').all()
     serializer_class = WorkerSerializer
 
+
+class WorkerView(viewsets.GenericViewSet):
+    serializer_class = WorkerSerializer
+    queryset = User.objects.all()
+
+    @action(detail=True, methods=['get'])
+    def enter_queue(self, request, pk):
+        worker = self.get_object()
+        queue, _ = WorkerQueue.objects.get_or_create(worker=worker)
+        queue.clients.add(request.user)
+        return Response({'success': True})
+
+    @action(detail=True, methods=['get'])
+    def quit_queue(self, request):
+        worker = self.get_object()
+        queue, _ = WorkerQueue.objects.get_or_create(worker=worker)
+        queue.clients.remove(request.user)
+        return Response({'success': True})
 
 
